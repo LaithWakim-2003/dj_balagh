9e9013a27c0f35704afa2fc2a87d051415d5907c
LaithWakim-2003 <laithwakimk@gmail.com>
2025-02-27 11:17:06 +0300

    Your commit message

--- balagh/urls.py
+++ balagh/urls.py
@@ -1,6 +1,10 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import re_path,include
 urlpatterns = [
     re_path('auth/', include('authenticate.urls')),
     re_path('location/',include('locations.urls')),
     re_path('report/',include('reports.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
